e1a31fc29e00efc8d4f8860850a6baf94ecb5fb6
gvasko79 <gabor.vaskoszedlar@gmail.com>
2016-01-24 19:34:46 +0100

    Uncomment for the prev commit

--- disposable/stack/build-server/build-server-provision.sh
+++ disposable/stack/build-server/build-server-provision.sh
@@ -13,23 +13,23 @@ fi
 
 echo "scriptdir=$scriptdir"
 
-#wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-#sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-#apt-get update
-#apt-get install -y openjdk-7-jdk
-#apt-get install -y jenkins
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y openjdk-7-jdk
+apt-get install -y jenkins
 
-#apt-get install -y nginx
-#rm /etc/nginx/sites-available/default
-#rm /etc/nginx/sites-enabled/default
+apt-get install -y nginx
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
 cp $scriptdir/nginx.config /etc/nginx/sites-available/jenkins
 ln -nfs /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
 mkdir -p /var/log/nginx/jenkins
 service nginx restart
 
-#jenkins_args=$(cat /etc/default/jenkins | grep JENKINS_ARGS | sed -e 's:\"$: --prefix=/jenkins\":')
-#cat /etc/default/jenkins | grep -v JENKINS_ARGS > /etc/default/jenkins.new
-#echo $jenkins_args >> /etc/default/jenkins.new
-#mv /etc/default/jenkins.new /etc/default/jenkins
+jenkins_args=$(cat /etc/default/jenkins | grep JENKINS_ARGS | sed -e 's:\"$: --prefix=/jenkins\":')
+cat /etc/default/jenkins | grep -v JENKINS_ARGS > /etc/default/jenkins.new
+echo $jenkins_args >> /etc/default/jenkins.new
+mv /etc/default/jenkins.new /etc/default/jenkins
 
-#service jenkins restart
+service jenkins restart
