459f0e4dec9efa66a2ffe3eae43a52b8eb233b1c
gvasko79 <gabor.vaskoszedlar@gmail.com>
2017-02-20 22:08:18 +0000

    set aws docker registry temporarily to run

--- disposable/stack/docker-build-environments/java-run.sh
+++ disposable/stack/docker-build-environments/java-run.sh
@@ -1,3 +1,4 @@
-docker run -dt -p 50022:22 --name jenkins-java-node-1 gvasko/jenkins-java-node
+docker pull 221820444680.dkr.ecr.eu-central-1.amazonaws.com/gvasko/jenkins-java-node
+docker run -dt -p 50022:22 --name jenkins-java-node-1 221820444680.dkr.ecr.eu-central-1.amazonaws.com/gvasko/jenkins-java-node
 docker ps
 
