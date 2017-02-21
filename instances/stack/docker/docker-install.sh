430d6cf49fab9b6e0fbbd5c9b54faa341ba02899
gvasko79 <gabor.vaskoszedlar@gmail.com>
2017-02-21 23:21:14 +0000

    trying to start jenkins build node using docker containers on-demand

--- instances/stack/docker/docker-install.sh
+++ instances/stack/docker/docker-install.sh
@@ -3,6 +3,7 @@ yum update -y
 yum install -y docker
 #echo "DOCKER_OPTS='-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock'" >> /etc/init/docker.conf
 #TODO: /etc/sysconfig/docker and modify the OPTION varaible
+# and see that docker daemon is executed by root, so docker login to the private registry should run under it
 service docker start
 usermod -a -G docker ec2-user
 
