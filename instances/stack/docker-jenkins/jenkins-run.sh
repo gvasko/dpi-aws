695a90c69793a459448ef1e448ad9d8c8076bb17
gvasko79 <gabor.vaskoszedlar@gmail.com>
2017-02-21 20:56:31 +0100

    jenkins instance: use image

--- instances/stack/docker-jenkins/jenkins-run.sh
+++ instances/stack/docker-jenkins/jenkins-run.sh
@@ -11,14 +11,15 @@ echo "scriptdir=$scriptdir"
 
 source $scriptdir/common.include
 
-jenkins_home_dir="jenkins_home"
+jenkins_home_dir=$scriptdir/jenkins_home
 
 mkdir $jenkins_home_dir
 
 uid=$(id -u)
 gid=$(id -g)
 
-docker run -dt -p 80:8080 -v $PWD/$jenkins_home_dir:/var/jenkins_home:z --name jenkins --user $uid:$gid gvasko/jenkins
+docker pull $DOCKER_PRIVATE_REGISTRY/gvasko/jenkins-java-node
+docker run -dt -p 80:8080 -v $scriptdir/$jenkins_home_dir:/var/jenkins_home:z --name jenkins --user $uid:$gid $DOCKER_PRIVATE_REGISTRY/gvasko/jenkins
 
 docker ps
 
