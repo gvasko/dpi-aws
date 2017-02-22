c5125f24ce47abaea0c0bbf7ba6ecb5a40fd99fc
gvasko79 <gabor.vaskoszedlar@gmail.com>
2017-02-22 22:56:22 +0100

    Set plugins version, safer credentials setup

--- images/stack/docker-jenkins/init.groovy.d/credentials.groovy
+++ images/stack/docker-jenkins/init.groovy.d/credentials.groovy
@@ -24,15 +24,23 @@ try {
 	// 	nodeProperties.load(it)
 	// }
 
+	def userId = 'JENKINS_USER_ID'
 	def paramUsername = 'jenkins'     //nodeProperties.'BUILD_NODE_USER'
 	def paramPassword = 'jenkins'     //nodeProperties.'BUILD_NODE_PASSWORD'
 
     def credentials_store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-    def scope = CredentialsScope.GLOBAL
-    def credentials = new UsernamePasswordCredentialsImpl(scope, 'JENKINS_USER_ID', 'Jenkins User for the Attached Nodes', paramUsername, paramPassword)
     def global = Domain.global()
-    credentials_store.addCredentials(global, credentials)
-    LOGGER.info('#DPI: Credentials - successfully created')
+
+	def alreadyCreated = credentials_store.getCredentials(global).find { it.getId() == userId } != null
+
+	if (alreadyCreated) {
+	    LOGGER.info('#DPI: Credentials - already created')
+	} else {
+	    def scope = CredentialsScope.GLOBAL
+	    def credentials = new UsernamePasswordCredentialsImpl(scope, userId, 'Jenkins User for the Attached Nodes', paramUsername, paramPassword)
+	    credentials_store.addCredentials(global, credentials)
+	    LOGGER.info('#DPI: Credentials - successfully created')
+	}
 }
 catch (Exception e) {
     LOGGER.warning('#DPI: Credentials - exception: ' + e.getClass().getName())
