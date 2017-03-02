33d68a04222b11f6f195e85e5e1d4d77f2f18c76
gvasko79 <gabor.vaskoszedlar@gmail.com>
2017-03-02 23:18:20 +0100

    Start/stop docker node too

--- instances/stack/jenkins-run.sh
+++ instances/stack/jenkins-run.sh
@@ -9,6 +9,7 @@ fi
 
 echo "scriptdir=$scriptdir"
 
+$scriptdir/docker-build-environments/docker-run.sh
 $scriptdir/docker-build-environments/java-run.sh
 $scriptdir/docker-build-environments/nodejs-run.sh
 $scriptdir/docker-jenkins/jenkins-run.sh
--- instances/stack/jenkins-stop.sh
+++ instances/stack/jenkins-stop.sh
@@ -9,6 +9,7 @@ fi
 
 echo "scriptdir=$scriptdir"
 
+$scriptdir/docker-build-environments/docker-stop.sh
 $scriptdir/docker-build-environments/java-stop.sh
 $scriptdir/docker-build-environments/nodejs-stop.sh
 $scriptdir/docker-jenkins/jenkins-stop.sh
